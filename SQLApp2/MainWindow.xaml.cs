b8f7b9badb4b5831e73840ff3963bd9c97fc6dc4
PhumipatWave <83903422+PhumipatWave@users.noreply.github.com>
2021-09-14 17:38:05 +0700

    Add project files.

---
+++ SQLApp2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+            DataAccess.AddData("Phumipat Sawasdee");
+        }
+
+        private void ShowBt_Click(object sender, RoutedEventArgs e)
+        {
+            string Value = " ";
+            foreach(string entry in DataAccess.GetData())
+            {
+                Value = Value + entry + "\n";
+            }
+            MessageBox.Show(Value);
+        }
+    }
+}
